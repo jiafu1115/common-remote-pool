d668303950c2261cc3e771443cdc3f93ceb9eae5
fujian1115@gmail.com <fujian1115@gmail.com@f46bdf04-c874-7f4f-7349-98ae1023fbda>
2014-02-21 07:37:59 +0000

    add exception

---
+++ common-remote-pool/src/main/java/com/googlecode/common/remote/pool/exception/ResourceFactoryClassNoUploadException.java
@@ -0,0 +1,7 @@
+package com.googlecode.common.remote.pool.exception;
+
+ @SuppressWarnings("serial")
+public class ResourceFactoryClassNoUploadException extends UncheckedCommonRemotePoolException{
+
+
+}
