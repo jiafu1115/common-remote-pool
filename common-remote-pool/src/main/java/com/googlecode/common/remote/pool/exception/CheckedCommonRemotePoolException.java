4f74695f8f2fc3f5efcab217923dabd73f6751de
fujian1115 <fujian1115@gmail.com>
2014-02-21 07:37:59 +0000

    add exception

---
+++ common-remote-pool/src/main/java/com/googlecode/common/remote/pool/exception/CheckedCommonRemotePoolException.java
@@ -0,0 +1,11 @@
+package com.googlecode.common.remote.pool.exception;
+
+public class CheckedCommonRemotePoolException extends Exception {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+
+}
---
+++ common-remote-pool/src/main/java/com/googlecode/common/remote/pool/exception/UncheckedCommonRemotePoolException.java
@@ -0,0 +1,11 @@
+package com.googlecode.common.remote.pool.exception;
+
+public class UncheckedCommonRemotePoolException extends RuntimeException {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+
+}
