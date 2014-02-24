737a25d33860a971f3e7afefac3c6457abeb80e7
fujian1115@gmail.com <fujian1115@gmail.com@f46bdf04-c874-7f4f-7349-98ae1023fbda>
2014-02-24 08:10:00 +0000

    add upload features

---
+++ common-remote-pool/src/main/java/com/googlecode/common/remote/pool/resource/upload/FileUploadForm.java
@@ -0,0 +1,23 @@
+package com.googlecode.common.remote.pool.resource.upload;
+
+import javax.ws.rs.FormParam;
+
+
+public class FileUploadForm {
+
+	public FileUploadForm() {
+	}
+
+	private byte[] data;
+
+	public byte[] getData() {
+		return data;
+	}
+
+	@FormParam("file")
+	public void setData(byte[] data) {
+		this.data = data;
+	}
+
+
+}
